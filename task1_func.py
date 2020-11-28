1762383efd86ab5e8438435a509d7c13005bcaa1
Roman Antonyuk <roman2062010@yandex.ru>
2020-11-29 00:51:28 +0500

    Задание №1: функции

---
+++ task1_func.py
@@ -0,0 +1,13 @@
+
+def get_summ(one, two, delimiter='&'):
+    two = two.capitalize()
+    return one + ' ' + delimiter + ' ' + two
+    
+one = "Learn" 
+two = "python"  
+print(get_summ(one, two))
+
+    
+
+   
+
