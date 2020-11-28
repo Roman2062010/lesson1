2b163f667408990dbb3926c894f013cfb649782a
Roman Antonyuk <roman2062010@yandex.ru>
2020-11-28 19:40:35 +0500

    Задание №2: словари

---
+++ task2_dict.py
@@ -0,0 +1,16 @@
+a = {"city": "Москва", "temperature": "20"}
+print(a["city"])
+a["temperature"] = int(a["temperature"]) - 5
+print(a["temperature"])
+print(a)
+
+if "country" in a: #способ №0
+    print(True)
+else:
+    print(False)
+
+print(a.get("country")) #способ №1
+
+a["country"] = "Россия"
+a["date"] = "27.05.2019" 
+print(len(a))
